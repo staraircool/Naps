9db863d5fcdf6c9e20d3915ee70dad4ed01043b3
Staraircool <staraircool@example.com>
2025-07-26 15:33:28 -0400

    Fix missing HomeScreen import in onboarding_screen.dart

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'home_screen.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
